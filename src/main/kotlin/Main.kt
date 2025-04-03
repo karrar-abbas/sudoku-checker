0fcc6ce84c3ad387db62e6c6f9d3149b915c3ed5
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-03 23:20:44 +0300

    implement sudokuCheck fun for 9x9 board

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
 
 fun main() {
 
-
 }
 
-fun sudokuCheck(sudokugrid:List<List<String>>)= false
+fun sudokuCheck(sudokuGrid:List<List<String>>): Boolean {
+    val subgrids= List(9){ mutableListOf<String>()}
+    var subgridIndex: Int
+
+    if (!checkDimensionsAndItems(sudokuGrid)) return false
+
+     sudokuGrid.forEachIndexed { rowIndex,row ->
+        row.forEachIndexed { columnIndex, item ->
+
+            if(row.count {it ==item && item in "1".."9" } >1
+                && sudokuGrid.drop(rowIndex).all{
+                    it[columnIndex] ==item && item in "1".."9" }) return false
+
+            else {
+                subgridIndex = (rowIndex / 3) * 3 + (columnIndex / 3)
+                if (item != "-" && subgrids[subgridIndex].contains(item)) return false
+                else if (item != "-") subgrids[subgridIndex].add(item)
+            }
+        }
+     }
+    return true
+}
+
+fun checkDimensionsAndItems(grid:List<List<String>>):Boolean{
+    return grid.size ==9 &&
+            grid.all { row->row.size ==9} &&
+            grid.all { row -> row.all {
+                item -> item in "1".."9" || item =="-" } }
+}
