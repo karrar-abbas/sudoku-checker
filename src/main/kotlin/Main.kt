a0c258d2f67da875c1df9904a1d40b1f626b83ac
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-04 18:51:15 +0300

    reimplement sudokuCheck fun to check 4,16 boards

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,52 @@
+import kotlin.math.sqrt
 
 fun main() {
 
 }
 
 fun sudokuCheck(sudokuGrid:List<List<String>>): Boolean {
-    val subgrids= List(9){ mutableListOf<String>()}
-    var subgridIndex: Int
+    val subgridSize = sqrt(sudokuGrid.size.toDouble()).toInt()
+    if (!checkDimensionsAndItems(sudokuGrid,subgridSize)) return false
 
-    if (!checkDimensionsAndItems(sudokuGrid)) return false
+    val subgridsItems= List(sudokuGrid.size){ mutableListOf<String>()} //change from 9
+    var subgridIndex: Int
 
      sudokuGrid.forEachIndexed { rowIndex,row ->
         row.forEachIndexed { columnIndex, item ->
 
-            if(row.count {it ==item && item in "1".."9" } >1
-                && sudokuGrid.drop(rowIndex).all{
-                    it[columnIndex] ==item && item in "1".."9" }) return false
+            when(sudokuGrid.size) {
+                4 -> { if (!checkFourOrNineRowsAndColumns(sudokuGrid,row,rowIndex,columnIndex,item,"4")) return false }
+                9 ->{ if (!checkFourOrNineRowsAndColumns(sudokuGrid,row,rowIndex,columnIndex,item,"9")) return false }
+                16 -> { if(!checkSixteenBoardRowsAndColumns(sudokuGrid,row,rowIndex,columnIndex,item)) return false } }
 
-            else {
-                subgridIndex = (rowIndex / 3) * 3 + (columnIndex / 3)
-                if (item != "-" && subgrids[subgridIndex].contains(item)) return false
-                else if (item != "-") subgrids[subgridIndex].add(item)
-            }
+            subgridIndex = (rowIndex /subgridSize) *subgridSize  + (columnIndex / subgridSize)
+            if (item != "-" && subgridsItems[subgridIndex].contains(item)) return false
+            else if (item != "-") subgridsItems[subgridIndex].add(item)
         }
      }
     return true
 }
 
-fun checkDimensionsAndItems(grid:List<List<String>>):Boolean{
-    return grid.size ==9 &&
-            grid.all { row->row.size ==9} &&
-            grid.all { row -> row.all {
-                item -> item in "1".."9" || item =="-" } }
+fun checkDimensionsAndItems(grid:List<List<String>>,subgridSize:Int):Boolean{
+
+    if(subgridSize * subgridSize !=grid.size || grid.any { row->row.size != grid.size}) return false
+
+    when(grid.size) {
+        4 -> {return grid.all { row -> row.all { item -> item in "1".."4" || item =="-" } }}
+        9 -> {return grid.all { row -> row.all { item -> item in "1".."9" || item =="-" } }}
+        16 -> {return grid.all { row -> row.all { item -> item in "1".."9" || item in "A".."G" || item =="-" } }}
+        else -> return false
+    }
+
 }
+fun checkFourOrNineRowsAndColumns(sudokuGrid: List<List<String>>, row:List<String>, rowIndex:Int,columnIndex:Int, item:String, maxNumber:String):Boolean{
+    if(row.count {it ==item && item in "1"..maxNumber } >1 ) return false
+    if(sudokuGrid.drop(rowIndex+1).any{ it[columnIndex] ==item && item in "1"..maxNumber }) return false
+    return true
+}
+fun checkSixteenBoardRowsAndColumns(sudokuGrid: List<List<String>>, row:List<String>, rowIndex:Int,columnIndex:Int, item:String):Boolean{
+    if(row.count {it ==item && (item in "1".."9" || item in "A".."G") } >1 ) return false
+    if(sudokuGrid.drop(rowIndex+1).any{ it[columnIndex] ==item && (item in "1".."9" || item in "A".."G" )}) return false
+    return true
+}
+
